df23679f5b3832020ad3ea857956f8363209938a
Cystian <135488065+Cystian@users.noreply.github.com>
2025-08-05 05:17:28 -0500

    Update router.jsx

--- src/router.jsx
+++ src/router.jsx
@@ -1,6 +1,6 @@
 // src/router/router.jsx
 import { createBrowserRouter } from "react-router-dom";
-import MainLayout from "./layout/MainLayout";
+import MainLayout from "./Layout/MainLayout";
 import Home from "./pages/Home";
 import Properties from "./pages/Properties";
 import Login from "./pages/Login";
