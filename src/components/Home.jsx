4a59331a339808de615d3f84629e4003a0504386
Cystian <135488065+Cystian@users.noreply.github.com>
2025-08-07 08:52:31 -0500

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,16 @@
 // src/pages/Home.jsx
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
+import SearchBanner from '../components/SearchBanner';
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <Hero />
+       <div className="mt-12">
+      <SearchBanner />
+     
     </>
   );
 };
