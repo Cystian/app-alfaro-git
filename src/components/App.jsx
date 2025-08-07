d978eb06e108c61d8081111e92e35dc875fb5d68
Cystian <135488065+Cystian@users.noreply.github.com>
2025-08-07 09:21:06 -0500

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,8 @@
 // src/App.jsx
 import Home from './pages/Home';
-import SearchBanner from './components/SearchBanner';
 
 function App() {
   return <Home />;
-   <div className="mt-12">
-      <SearchBanner />
-      </div>
 }
 
 export default App;
