f0d44c20eadec2ba9fb12a812a228dcd5ca55465
Cystian <135488065+Cystian@users.noreply.github.com>
2025-08-09 17:11:12 -0500

    Actualizar MainLayout.jsx

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,3 +1,4 @@
+// src/layouts/MainLayout.jsx
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
@@ -6,10 +7,14 @@ import { Outlet } from 'react-router-dom';
 const MainLayout = () => {
   return (
     <div className="flex flex-col min-h-screen">
+      {/* Navbar fijo arriba con espacio reservado */}
       <Navbar />
-      <main className="flex-grow">
+      
+      {/* Contenido principal con padding adaptativo */}
+      <main className="flex-grow w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-20 sm:pt-24">
         <Outlet />
       </main>
+      
       <Footer />
     </div>
   );
