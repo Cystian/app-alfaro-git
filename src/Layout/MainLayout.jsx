ecc69f75c2599b84b7c4f038d647327a0bdd1eb9
Cystian <135488065+Cystian@users.noreply.github.com>
2025-08-10 08:12:14 -0500

    Actualizar MainLayout.jsx

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,49 +1,16 @@
 // src/layouts/MainLayout.jsx
-import React, { useEffect } from 'react';
+import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import { Outlet } from 'react-router-dom';
 
 const MainLayout = () => {
-  useEffect(() => {
-    // Facebook SDK
-    if (!document.getElementById('facebook-jssdk')) {
-      const fbScript = document.createElement('script');
-      fbScript.id = 'facebook-jssdk';
-      fbScript.src = 'https://connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v16.0';
-      fbScript.async = true;
-      fbScript.defer = true;
-      fbScript.crossOrigin = 'anonymous';
-      document.body.appendChild(fbScript);
-    }
-
-    // Instagram embed
-    if (!document.querySelector('script[src="//www.instagram.com/embed.js"]')) {
-      const igScript = document.createElement('script');
-      igScript.src = '//www.instagram.com/embed.js';
-      igScript.async = true;
-      document.body.appendChild(igScript);
-    }
-
-    // TikTok embed
-    if (!document.querySelector('script[src="https://www.tiktok.com/embed.js"]')) {
-      const ttScript = document.createElement('script');
-      ttScript.src = 'https://www.tiktok.com/embed.js';
-      ttScript.async = true;
-      document.body.appendChild(ttScript);
-    }
-  }, []);
-
   return (
     <div className="flex flex-col min-h-screen">
-      {/* Navbar fijo arriba */}
       <Navbar />
-
-      {/* Contenido principal */}
       <main className="flex-grow w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-20 sm:pt-24">
         <Outlet />
       </main>
-
       <Footer />
     </div>
   );
