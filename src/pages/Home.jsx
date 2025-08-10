340eebd1212531b71df23dadc483550a63012125
Cystian <135488065+Cystian@users.noreply.github.com>
2025-08-09 19:51:23 -0500

    Actualizar Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -44,7 +44,7 @@ export default function Home() {
 
         {/* Propiedades destacadas */}
         <section className="w-full">
-          <h2 className="text-2xl font-bold mb-6 text-gray-800">
+          <h2 className="text-2xl font-bold mb-6 text-gray-800 mt-20 ">
             Propiedades destacadas
           </h2>
           <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
