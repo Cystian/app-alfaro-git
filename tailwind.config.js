13a9619842d73c2998b9230b3ca3719296cfdb7a
Cystian <135488065+Cystian@users.noreply.github.com>
2025-08-11 10:36:55 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -53,3 +53,23 @@ export default {
   },
   plugins: [],
 }
+
+module.exports = {
+  theme: {
+    extend: {
+      animation: {
+        'gradient-move': 'gradientBG 4s ease infinite',
+      },
+      keyframes: {
+        gradientBG: {
+          '0%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+          '100%': { backgroundPosition: '0% 50%' },
+        },
+      },
+      backgroundSize: {
+        '300%': '300% 300%',
+      },
+    },
+  },
+};
