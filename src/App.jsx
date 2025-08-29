223939f6c0535e7638fa1ee5a869b9b0f525a797
Cystian <135488065+Cystian@users.noreply.github.com>
2025-08-29 11:16:34 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./router"; // asumiendo que tu archivo está en src/router.jsx
 import "./styles/main.css";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   return (
     <div className="font-sans bg-gray-100 min-h-screen text-gray-800">
       <RouterProvider router={router} />
+      {/* 🔔 Contenedor global de notificaciones */}
+      <Toaster position="top-center" reverseOrder={false} />
     </div>
   );
 };
 
 export default App;
+
