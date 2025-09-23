09c72daef5ea9b4d5b38c8c940c454696ab76306
Cystian <135488065+Cystian@users.noreply.github.com>
2025-09-22 19:51:49 -0500

    Update PropertyModal.jsx

--- src/components/PropertyModal.jsx
+++ src/components/PropertyModal.jsx
@@ -295,12 +295,17 @@ const PropertyModal = ({ property, onClose }) => {
                 </button>
 
                 {/* Botón Resumen Completo */}
- <Link to={`/propiedades/resumen/${propData.id}`}>
+<a
+  href={`/propiedades/resumen/${propData.id}`}
+  target="_blank"
+  rel="noopener noreferrer"
+>
   <button className="inline-flex items-center bg-gray-800 text-white py-2 px-4 rounded-lg hover:bg-gray-900 transition">
     <img src="/documento.png" alt="Resumen" className="w-5 h-5 mr-2" />
     Ver Resumen
   </button>
-</Link>
+</a>
+
                 
               </div>
             </div>
