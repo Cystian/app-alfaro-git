dfa2f556925597d28628ed14bf22b609b28be7cf
Cystian <135488065+Cystian@users.noreply.github.com>
2025-09-22 19:34:20 -0500

    Delete src/utils/generateResumenCompleto.js

--- src/utils/generateResumenCompleto.js
+++
@@ -1,7 +0,0 @@
-export const generateResumenCompleto = (propData) => {
-  // Guardar objeto en sessionStorage
-  sessionStorage.setItem("propDataResumen", JSON.stringify(propData));
-
-  // Abrir nueva pestaña
-  window.open("/propiedades/resumen", "_blank");
-};
