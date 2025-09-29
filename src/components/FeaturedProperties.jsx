a805138f020d93305fd207e1319b17f7088bc189
Cystian <135488065+Cystian@users.noreply.github.com>
2025-09-29 09:47:17 -0500

    Update FeaturedProperties.jsx

--- src/components/FeaturedProperties.jsx
+++ src/components/FeaturedProperties.jsx
@@ -1,6 +1,8 @@
 // ✅ Carrusel de propiedades destacadas: siempre 6 más recientes
 import React, { useEffect, useState } from "react";
-import Slider from "react-slick";
+import Slider from "react-slick"; // Swiper también es válido
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function FeaturedProperties() {
   const [properties, setProperties] = useState([]);
