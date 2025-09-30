65a3c474ffb2f1dcab44e16660ca2a97bcaa6d50
Cystian <135488065+Cystian@users.noreply.github.com>
2025-09-30 10:01:18 -0500

    Update PropertyResumenPage.jsx

--- src/pages/PropertyResumenPage.jsx
+++ src/pages/PropertyResumenPage.jsx
@@ -185,7 +185,7 @@ export default function PropertyResumenPage() {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" className="w-5 h-5">
                   <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
                 </svg>
-                Buscar más propiedades
+                Buscar más propiedadesX
               </button>
             </div>
           </div>
