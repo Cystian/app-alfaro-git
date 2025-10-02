2dd59cd0da049b616a8e7d4838396e7930e6f534
Cystian <135488065+Cystian@users.noreply.github.com>
2025-10-02 09:58:41 -0500

    Update MainLayout.jsx

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
-import GuiaButton from "./components/GuiaButton";
+import GuiaButton from "../components/GuiaButton";
 import { Outlet } from 'react-router-dom';
 
 const MainLayout = () => {
