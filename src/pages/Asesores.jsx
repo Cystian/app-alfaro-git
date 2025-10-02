d66bb6397ef61921b72b8f157c48271951bfe9e1
Cystian <135488065+Cystian@users.noreply.github.com>
2025-10-02 15:40:50 -0500

    Create Asesores.jsx

---
+++ src/pages/Asesores.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Asesores = () => {
+  return (
+    <section className="px-6 py-10 max-w-5xl mx-auto">
+      <h1 className="text-3xl font-bold text-center mb-6">Nuestro Blog</h1>
+      <p className="text-gray-700 text-lg text-center">
+        Lista de Asesores
+      </p>
+    </section>
+  );
+};
+
+export default Asesores;
