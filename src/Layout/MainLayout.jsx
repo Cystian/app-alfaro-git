aa8668def68f03edd055fb7969b1e1b40de85fb0
Cystian <135488065+Cystian@users.noreply.github.com>
2025-10-02 09:55:39 -0500

    Update MainLayout.jsx

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -2,12 +2,14 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import GuiaButton from "./components/GuiaButton";
 import { Outlet } from 'react-router-dom';
 
 const MainLayout = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
+       <GuiaButton />
       <main className="flex-grow w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-20 sm:pt-24">
         <Outlet />
       </main>
