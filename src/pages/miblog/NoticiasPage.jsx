c116da7c70de948fa3717c313476a6f6fee8ceae
Cystian <135488065+Cystian@users.noreply.github.com>
2025-10-07 09:42:34 -0500

    Create NoticiasPage.jsx

---
+++ src/pages/miblog/NoticiasPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import NoticiasGrid from "./NoticiasGrid";
+
+const NoticiasPage = () => {
+  return <NoticiasGrid newsList={sampleNews} />;
+};
+
+export default NoticiasPage;
