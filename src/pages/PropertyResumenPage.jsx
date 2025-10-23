a3621a0192cefa7944f95225cb82e227f39a6cdd
Cystian <135488065+Cystian@users.noreply.github.com>
2025-10-23 04:32:24 -0500

    Update PropertyResumenPage.jsx

--- src/pages/PropertyResumenPage.jsx
+++ src/pages/PropertyResumenPage.jsx
@@ -260,12 +260,10 @@ if (!data) {
 
         <div className="flex items-center bg-gray-50 p-5 rounded-2xl shadow-md border border-gray-200 hover:shadow-lg transition-all duration-300">
     <FaRulerCombined className="text-rojo-inmobiliario mr-3 text-2xl" />
-    <div>
+   
       <p className="text-gray-500 text-sm">Dimensiones</p>
       
-    </div>
 
-  <div className="space-y-1">
           <p className="text-gray-600 text-base">
             <span className="font-medium text-negro-profundo">Frente:</span>{" "}
             {data.property.frontera
@@ -278,7 +276,7 @@ if (!data) {
               ? `${data.property.largo} m`
               : "No especificado"}
           </p>
-        </div>
+        
           
   </div>
       
