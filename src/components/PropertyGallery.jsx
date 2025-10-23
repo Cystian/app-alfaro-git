76026a2c8a414fb89ac7accf99d1ca2069645920
Cystian <135488065+Cystian@users.noreply.github.com>
2025-10-23 06:33:14 -0500

    Update PropertyGallery.jsx

--- src/components/PropertyGallery.jsx
+++ src/components/PropertyGallery.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import { Navigation, Pagination, Autoplay ,Thumbs } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
