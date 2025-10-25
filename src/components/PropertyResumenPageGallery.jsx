4d5173c6963f42b6a2bcba1f0773cd22b51c322b
Cystian <135488065+Cystian@users.noreply.github.com>
2025-10-24 22:45:54 -0500

    Actualizar PropertyResumenPageGallery.jsx

--- src/components/PropertyResumenPageGallery.jsx
+++ src/components/PropertyResumenPageGallery.jsx
@@ -1,7 +1,7 @@
 // src/components/PropertyResumePageGallery.jsx
 import React, { useState, useRef, useEffect } from "react";
 import { FaTimes, FaChevronLeft, FaChevronRight } from "react-icons/fa";
-import "../styles/PropertyResumePageGallery.css";
+import "../styles/PropertyResumenPageGallery.css";
 
 const PropertyResumePageGallery = ({ images, title, description }) => {
   const [isOpen, setIsOpen] = useState(false);
