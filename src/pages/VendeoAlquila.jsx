9204c6204ba2fba472f950f93958584733ffb292
Cystian <135488065+Cystian@users.noreply.github.com>
2025-10-28 14:58:40 -0500

    Update VendeoAlquila.jsx

--- src/pages/VendeoAlquila.jsx
+++ src/pages/VendeoAlquila.jsx
@@ -9,7 +9,7 @@ const VendeoAlquila = () => {
 
         {/* 🔻 Banner superior */}
         <section
-          className="w-full min-h-[300px] md:min-h-[400px] bg-center bg-no-repeat relative flex items-center justify-center py-10 md:py-10"
+          className="w-full min-h-[300px] md:min-h-[500px] bg-center bg-no-repeat relative flex items-center justify-center py-10 md:py-10"
           style={{
             backgroundImage: "url('/subtitulos/banner_vendeoalquila.png')",
             backgroundSize: "cover",
