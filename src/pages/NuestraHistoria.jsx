207e8b3ed6c93583c0c5310c1a14e838971be387
Cystian <135488065+Cystian@users.noreply.github.com>
2025-10-29 10:30:11 -0500

    Update NuestraHistoria.jsx

--- src/pages/NuestraHistoria.jsx
+++ src/pages/NuestraHistoria.jsx
@@ -4,7 +4,9 @@ import { Building2, Users2, Star, Landmark } from "lucide-react";
 
 const NuestraHistoria = () => {
   return (
-    <section className="relative bg-[#F9F9F9] px-6 md:px-20 overflow-hidden">
+    <section className="relative bg-[#F9F9F9] px-6 md:px-20 overflow-hidden" style={{
+    padding-top: 2rem;}}
+">
       {/* Fondo decorativo */}
       <div className="absolute inset-0 bg-cover bg-center opacity-10" />
 
@@ -179,4 +181,5 @@ const NuestraHistoria = () => {
 };
 
 export default NuestraHistoria;
+
 
