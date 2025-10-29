0357eabfd29b0b67ed2c743681096db6a42def25
Cystian <135488065+Cystian@users.noreply.github.com>
2025-10-29 14:57:35 -0500

    Update NuestraHistoria.jsx

--- src/pages/NuestraHistoria.jsx
+++ src/pages/NuestraHistoria.jsx
@@ -18,7 +18,7 @@ const NuestraHistoria = () => {
         {/* === Banner Principal === */}
         <div className="relative w-full h-auto md:h-[420px] overflow-hidden bg-[#F5F5F5]">
           <img
-            src="/nuestra-historia.png"
+            src="/n_historia.png"
             alt="Banner Nuestra Historia"
             className="w-full h-auto md:h-[420px] object-contain mx-auto"
           />
@@ -179,6 +179,7 @@ const NuestraHistoria = () => {
 };
 
 export default NuestraHistoria;
+
 
 
 
