7c7b75e1353d1d1672f42fd9a517e977f261f0c6
Cystian <135488065+Cystian@users.noreply.github.com>
2025-11-11 15:09:42 -0500

    Update MainLayout.jsx

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -9,7 +9,7 @@ const MainLayout = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-       <GuiaButton />
+     //   GuiaButton 
       <main className="flex-grow w-full max-w-9xl mx-auto px-0 sm:px-0 lg:px-0 pt-20 sm:pt-24 -mt-12">
         <Outlet />
       </main>
