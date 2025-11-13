9843dd180fb093f806b4fa5862278313358f97d1
Cystian <135488065+Cystian@users.noreply.github.com>
2025-11-13 15:07:13 -0500

    Create SimpleLayout.jsx

---
+++ src/Layout/SimpleLayout.jsx
@@ -0,0 +1,12 @@
+import Footer from "../../components/Footer";
+
+const SimpleLayout = ({ children }) => {
+  return (
+    <div className="w-full min-h-screen flex flex-col">
+      <main className="flex-grow">{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+export default SimpleLayout;
