df05fa15606b581e86da569363b4db249bc934c0
Cystian <135488065+Cystian@users.noreply.github.com>
2025-11-13 15:30:27 -0500

    Adjust height values for PropertyGallery component

--- src/components/PropertyGallery.jsx
+++ src/components/PropertyGallery.jsx
@@ -50,7 +50,7 @@ export default function PropertyGallery({ data }) {
           font-sans relative
           mt-[-2rem]
           left-1/2 -translate-x-1/2
-          h-[380px] md:h-[430px] lg:h-[480px]
+          h-[410px] md:h-[460px] lg:h-[510px]
           overflow-hidden
         "
       >
@@ -71,7 +71,7 @@ export default function PropertyGallery({ data }) {
             {images.map((img, index) => (
               <SwiperSlide key={index}>
                 <div
-                  className="relative w-full h-[360px] md:h-[410px] lg:h-[460px] overflow-hidden rounded-2xl bg-white flex items-center justify-center cursor-pointer"
+                  className="relative w-full h-[390px] md:h-[440px] lg:h-[490px] overflow-hidden rounded-2xl bg-white flex items-center justify-center cursor-pointer"
                   onClick={() => {
                     setCurrentIndex(index);
                     setLightboxOpen(true);
