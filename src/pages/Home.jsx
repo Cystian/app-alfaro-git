78d75b898f1bb75e34156a0992c84eabb68e6b6a
Cystian <135488065+Cystian@users.noreply.github.com>
2025-11-15 10:19:11 -0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -34,7 +34,7 @@ export default function Home() {
   // 🚀 AUTO-SCROLL cuando llegan resultados (con offset)
   setTimeout(() => {
     if (resultsRef.current) {
-      const offset = 120; // espacio extra hacia abajo
+      const offset = 80; // espacio extra hacia abajo
       const top = resultsRef.current.offsetTop - offset;
 
       window.scrollTo({
