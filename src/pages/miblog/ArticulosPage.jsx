8a0803fb58cca4001aac1d64b44ce8dfb6533286
Cystian <135488065+Cystian@users.noreply.github.com>
2025-11-30 10:36:56 -0500

    Update ArticulosPage.jsx

--- src/pages/miblog/ArticulosPage.jsx
+++ src/pages/miblog/ArticulosPage.jsx
@@ -7,7 +7,7 @@ import FloatingShare from "../../components/FloatingShare";
 const ArticulosPage = () => {
   return (
     <>
-    <FloatingShare />
+  
     <div className="max-w-6xl mx-auto px-4 py-8">
       {/* Botón Volver al Blog */}
       <div className="mb-6">
