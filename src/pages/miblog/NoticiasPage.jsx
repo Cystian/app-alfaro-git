d3e3bbea7e9d750521b24cd69a15723cf2beed61
Cystian <135488065+Cystian@users.noreply.github.com>
2025-11-30 10:33:57 -0500

    Update NoticiasPage.jsx

--- src/pages/miblog/NoticiasPage.jsx
+++ src/pages/miblog/NoticiasPage.jsx
@@ -14,7 +14,7 @@ const NoticiasPage = () => {
       <div className="mb-6">
         <Link
           to="/blog"
-          className="inline-flex items-center text-red-500 hover:text-red-700 transition font-medium"
+          className="inline-flex items-center text-[#DF011A] hover:text-red-700 transition font-medium"
         >
           <ArrowLeft className="w-5 h-5 mr-2" />
           Volver al Blog
