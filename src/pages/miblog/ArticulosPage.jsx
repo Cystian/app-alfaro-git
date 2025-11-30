092bc1c06e83ee6439cccb02f57dcc3a98fe7e90
Cystian <135488065+Cystian@users.noreply.github.com>
2025-11-30 10:34:18 -0500

    Update ArticulosPage.jsx

--- src/pages/miblog/ArticulosPage.jsx
+++ src/pages/miblog/ArticulosPage.jsx
@@ -13,7 +13,7 @@ const ArticulosPage = () => {
       <div className="mb-6">
         <Link
           to="/blog"
-          className="inline-flex items-center text-red-500 hover:text-red-700 transition font-medium"
+          className="inline-flex items-center text-[#DF011A] hover:text-red-700 transition font-medium"
         >
           <ArrowLeft className="w-5 h-5 mr-2" />
           Volver al Blog
